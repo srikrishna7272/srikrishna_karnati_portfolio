d65ee66ca8a000d899ab065efbd61d80ba97db3e
Srikrishna Karnati <105886453+srikrishna7272@users.noreply.github.com>
2023-10-15 22:17:44 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/about";
 import Contact from "@/components/contact";
+import Education from "@/components/education";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
@@ -14,6 +15,7 @@ export default function Home() {
       <About />
       <Projects />
       <Skills />
+      <Education />
       <Experience />
       <Contact />
     </main>
