a4adf516d687821f3ec59442d114cb2a7e5b79f3
Srikrishna Karnati <105886453+srikrishna7272@users.noreply.github.com>
2023-10-25 18:33:06 -0700

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -59,7 +59,7 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Srikrishna.</span> I'm a <span className="font-bold">Web developer</span>{" "}
-        with <span className="font-bold">2 years</span> of experience. I enjoy building{" "}
+        with <span className="font-bold">3 years</span> of experience. I enjoy building{" "}
         <span className="italic">websites</span>. My focus is <span className="underline">React and Next Js</span>.
       </motion.h1>
 
